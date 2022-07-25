29dd16e93f727cea460cfe79a445dd51e3c59766
FouadHijazi <fouad.o.hijazi@gmail.com>
2022-07-25 13:28:20 +0300

    create roters

---
+++ book-club/backend/routes/rooms.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const { } = require("../controllers/room");
+
+const roomsRouter = express.Router();
+
+roomsRouter.post("/", );
+
+module.exports = roomsRouter;
